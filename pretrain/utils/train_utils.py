3f99911d4cde52c51d8bb17b2e37f160b1057f63
Morales97 <danimoralesbrotons@gmail.com>
2022-03-21 15:03:05 +0100

    Update train_utils.py

--- pretrain/utils/train_utils.py
+++ pretrain/utils/train_utils.py
@@ -5,7 +5,7 @@
 import torch
 from torch.nn.functional import cross_entropy
 from utils.utils import AverageMeter, ProgressMeter, freeze_layers
-
+import pdb
 
 def train(p, train_loader, model, optimizer, epoch, amp):
     losses = AverageMeter('Loss', ':.4e')
