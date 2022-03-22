821f93d196b49bb14a8dff7f889eb2418c81db07
Morales97 <danimoralesbrotons@gmail.com>
2022-03-22 16:08:29 +0100

    .

--- pretrain/run_main.sh
+++ pretrain/run_main.sh
@@ -10,5 +10,5 @@
 #SBATCH --mem=30000
 #SBATCH --time=12:00:00
 
-python main.py --expt_name=test --config_env configs/env.yml --config_exp configs/VOCSegmentation_supervised_saliency_model.yml
-python main_single_thread.py --expt_name=test --config_env configs/env.yml --config_exp configs/VOCSegmentation_supervised_saliency_model.yml
+#python main.py --expt_name=test --config_env configs/env.yml --config_exp configs/VOCSegmentation_supervised_saliency_model.yml
+python main_single_thread.py --expt_name=test_no_acc --config_env configs/env.yml --config_exp configs/VOCSegmentation_supervised_saliency_model.yml
