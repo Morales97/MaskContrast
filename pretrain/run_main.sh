aafb9fb0c7f98b2081726ae0e730652be024b53b
Morales97 <danimoralesbrotons@gmail.com>
2022-03-25 10:25:39 +0100

    .

--- pretrain/run_main.sh
+++ pretrain/run_main.sh
@@ -16,9 +16,13 @@
 # cityscapes deeplab
 #python main_single_thread.py --expt_name=test --config_env configs/env.yml --config_exp configs/cityscapes.yml
 
+#cityscapes+GTA deeplab
+python main_single_thread.py --expt_name=CS_GTA_mn_deeplab --config_env configs/env.yml --config_exp configs/cityscapes_gta_deeplab.yml
+
+
 #cityscapes lraspp
 #python main_single_thread.py --expt_name=CS_mn_lraspp_ndim_19 --config_env configs/env.yml --config_exp configs/cityscapes_lraspp.yml
 
 #cityscapes+GTA lraspp
-python main_single_thread.py --expt_name=CS_GTA_mn_lraspp --config_env configs/env.yml --config_exp configs/cityscapes_gta_lraspp.yml
+#python main_single_thread.py --expt_name=CS_GTA_mn_lraspp --config_env configs/env.yml --config_exp configs/cityscapes_gta_lraspp.yml
 
