f94d9aecb250032e8d795e5749000bcd336df006
Morales97 <danimoralesbrotons@gmail.com>
2022-03-28 16:45:57 +0200

    .

--- pretrain/run_main.sh
+++ pretrain/run_main.sh
@@ -8,7 +8,7 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:2
 #SBATCH --mem=30000
-#SBATCH --time=12:00:00
+#SBATCH --time=16:00:00
 
 #python main.py --expt_name=test --config_env configs/env.yml --config_exp configs/VOCSegmentation_supervised_saliency_model.yml
 #python main_single_thread.py --expt_name=test_no_acc --config_env configs/env.yml --config_exp configs/VOCSegmentation_supervised_saliency_model.yml
@@ -17,11 +17,11 @@
 #python main_single_thread.py --expt_name=test --config_env configs/env.yml --config_exp configs/cityscapes.yml
 
 #cityscapes+GTA deeplab
-python main_single_thread.py --expt_name=CS_GTA_mn_deeplab --config_env configs/env.yml --config_exp configs/cityscapes_gta_deeplab.yml
+#python main_single_thread.py --expt_name=CS_GTA_mn_deeplab --config_env configs/env.yml --config_exp configs/cityscapes_gta_deeplab.yml
 
 
 #cityscapes lraspp
-#python main_single_thread.py --expt_name=CS_mn_lraspp_ndim_19 --config_env configs/env.yml --config_exp configs/cityscapes_lraspp.yml
+python main_single_thread.py --expt_name=CS_mn_lraspp_600_epochs --config_env configs/env.yml --config_exp configs/cityscapes_lraspp.yml
 
 #cityscapes+GTA lraspp
 #python main_single_thread.py --expt_name=CS_GTA_mn_lraspp --config_env configs/env.yml --config_exp configs/cityscapes_gta_lraspp.yml
