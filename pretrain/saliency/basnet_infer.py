fba50bb396d496c297707f2b2690f06442204e9d
Morales97 <danimoralesbrotons@gmail.com>
2022-03-30 17:11:35 +0200

    Update basnet_infer.py

--- pretrain/saliency/basnet_infer.py
+++ pretrain/saliency/basnet_infer.py
@@ -128,6 +128,6 @@ if __name__ == '__main__':
 		if _gta:
 			save_img(img_save_dir, name, image)
 			save_path = save_dir
-		save_output(save_dir, name, mask)
+		# save_output(save_dir, name, mask)
 
 	
