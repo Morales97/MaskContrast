dfa52673695fc4ccefd6003364bcd1662001b188
Morales97 <danimoralesbrotons@gmail.com>
2022-03-31 18:15:08 +0200

    Update common_config.py

--- pretrain/utils/common_config.py
+++ pretrain/utils/common_config.py
@@ -123,7 +123,7 @@ def get_train_dataset(p, transform=None, dataset=None, use_gt_masks=False):
         if use_gt_masks:
             return Gta(transform=transform, 
                        use_gt_masks=use_gt_masks,
-                       saliency=p['train_db_kwargs']['saliency_gt'],
+                       saliency=p['train_db2_kwargs']['saliency_gt'],
                        split='images_tiny_cropped_sup')   
         else:
             return Gta(transform=transform)
