cf0e7f50907ac0f3c842c36939fcd247f3f7ed5b
Morales97 <danimoralesbrotons@gmail.com>
2022-04-04 18:50:32 +0200

    .

--- pretrain/run_main.sh
+++ pretrain/run_main.sh
@@ -22,13 +22,14 @@
 
 #cityscapes lraspp
 #python main_single_thread.py --expt_name=CS_mn_lraspp_600_epochs --config_env configs/env.yml --config_exp configs/cityscapes_lraspp.yml
+python main_single_thread.py --expt_name=CS_mn_lraspp_no_IN_bb_part_2 --config_env configs/env.yml --config_exp configs/cityscapes_lraspp.yml
 
 #cityscapes+GTA lraspp
 #python main_single_thread.py --expt_name=CS_GTA_mn_lraspp_600_epochs --config_env configs/env.yml --config_exp configs/cityscapes_gta_lraspp.yml
 
 # ---- with supervised object masks -----
 #cityscapes lraspp
-python main_single_thread.py --expt_name=CS_100sup_no_unsup_no_sal_loss --config_env configs/env.yml --config_exp configs/cityscapes_lraspp.yml
+#python main_single_thread.py --expt_name=CS_100sup_no_unsup_no_sal_loss --config_env configs/env.yml --config_exp configs/cityscapes_lraspp.yml
 
 # CS + GTA lraspp
 #python main_single_thread.py --expt_name=CS_GTA_sup_masks --config_env configs/env.yml --config_exp configs/cityscapes_gta_lraspp.yml
