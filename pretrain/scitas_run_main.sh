7a7f8e18552e03412ba4186e1ab336f2ae38185f
Morales97 <danimoralesbrotons@gmail.com>
2022-04-12 14:42:36 +0200

    Update scitas_run_main.sh

--- pretrain/scitas_run_main.sh
+++ pretrain/scitas_run_main.sh
@@ -22,7 +22,7 @@
 
 
 #cityscapes lraspp
-python main_single_thread.py --expt_name=CS_mn_lraspp_no_IN_bb --config_env configs/env.yml --config_exp configs/cityscapes_lraspp.yml
+python main_single_thread.py --expt_name=CS_deeplab --config_env configs/env.yml --config_exp configs/cityscapes_deeplab.yml
 
 #cityscapes+GTA lraspp
 #python main_single_thread.py --expt_name=CS_GTA_mn_lraspp_600_epochs --config_env configs/env.yml --config_exp configs/cityscapes_gta_lraspp.yml
